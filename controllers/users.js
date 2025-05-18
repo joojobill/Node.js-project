698508e6ade5057433013649c9400b6f24025279
Isaac Hooper <idhamzzyisaac@gmail.com>
2025-05-18 22:34:29 +0000

    CHECK IN

--- controllers/users.js
+++ controllers/users.js
@@ -21,8 +21,63 @@ const getSingle = async (req , res) => {
 
 
 };
+
+const createUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        firstName: req.body.firstName,
+        lastname: req.body.lastname,
+        Email: req.body.Email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongobd.getDatabase().db().collection('users').insertOne(user);
+    if (response.acknowledge)  {
+        res.status(200).send();
+    } else {
+        res.status(500).json(response.error || 'some error occured while updating the user');
+    }
+};
+
+const updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        username: req.body.username,
+        email: req.body.email,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
+    };
+     const response = await mongobd.getDatabase().db().collection('users').replaceOne({_id: userId}, user);
+    if (response.modifiedCount > 0)  {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'some error occured while updating the user');
+    };
+    
+};
+const deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+
+    const user = {
+        username: req.body.username,
+        email: req.body.email,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
+    };
+     const response = await mongobd.getDatabase().db().collection('users').remove({_id: userId}, user);
+    if (response.deleteCount > 0)  {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'some error occured while deleting the user');
+    };
+
+};
+
  
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const mongobd = require('./data/database');
 const app = express();
 
+
+
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
 
 
